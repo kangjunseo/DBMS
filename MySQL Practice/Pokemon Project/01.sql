48b0cf306719e1176863b4f634916ba7ab35574a
KJS <88201041+kangjunseo@users.noreply.github.com>
2021-09-18 01:52:51 +0900

    Update 01.sql

--- MySQL Practice/Pokemon Project/01.sql
+++ MySQL Practice/Pokemon Project/01.sql
@@ -1,5 +1,3 @@
--- 01. Print the names of trainers that have caught 3 or more Pokémon in the order of the number of Pokémon caught.
-
 SELECT Trainer.name
 FROM Trainer, CatchedPokemon
 WHERE Trainer.id = CatchedPokemon.owner_id 
