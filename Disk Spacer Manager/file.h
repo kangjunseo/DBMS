0b9cfac1ef77f4cd6e570ef7743b303a05551c30
KJS <88201041+kangjunseo@users.noreply.github.com>
2021-09-27 17:04:54 +0900

    Update file.h

--- Disk Spacer Manager/file.h
+++ Disk Spacer Manager/file.h
@@ -1,40 +1,33 @@
-#include "file.h"
-#include <fstream>
-#include <cstdio>
-#include <fcntl.h>
-#include <unistd.h>
-#include <cerrno>
-#include <cstring>
-#define FILE_SIZE 10*1024*1024
-using namespace std;
+#include <stdint.h>
+#define PAGE_SIZE 4096
+typedef uint64_t pagenum_t;
+struct page_t {
+    // in-memory page structure
+    pagenum_t * next_page_t;
+    char reserved[PAGE_SIZE-sizeof(next_page_t)];
+};
+
+struct header_page_t {
+    // in-memory page structure
+    pagenum_t * first_page_t;
+    pagenum_t pages;
+    char reserved[PAGE_SIZE-sizeof(first_page_t)-sizeof(pages)];
+};
 
 // Open existing database file or create one if not existed.
-int64_t file_open_database_file(char* path){
-    int64_t db_file = open(path,O_SYNC|O_RDWR);   //Open the file from the path
-    if(db_file==-1) { //no file
-        ofstream ofs(path); //Create new file
-        if(truncate(path,FILE_SIZE)==-1){   //Set file size as 10MB
-            fprintf(stderr,"error: %s\n", strerror(errno));
-            return 1;
-        }
-        ofs.close();
-        db_file = open(path,O_SYNC|O_RDWR);
-    }
-    return db_file;
-}
+int64_t file_open_database_file(char* path);
 
 // Allocate an on-disk page from the free page list
-pagenum_t file_alloc_page(){
-    if()
-}
+pagenum_t file_alloc_page();
+
 // Free an on-disk page to the free page list
-void file_free_page(pagenum_t pagenum){
-}
-// Read an on-disk page into the in-memory page structure(dest) void file_read_page(pagenum_t pagenum, page_t* dest){
-}
+void file_free_page(pagenum_t pagenum);
+
+// Read an on-disk page into the in-memory page structure(dest)
+void file_read_page(pagenum_t pagenum, page_t* dest);
+
 // Write an in-memory page(src) to the on-disk page
-void file_write_page(pagenum_t pagenum, const page_t* src){
-}
+void file_write_page(pagenum_t pagenum, const page_t* src);
+
 // Stop referencing the database file
-void file_close_database_file(){
-}
+void file_close_database_file();
