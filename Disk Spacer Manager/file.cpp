801f1cec7681dbfed30c649daff0ebd021fbc6d5
KJS <88201041+kangjunseo@users.noreply.github.com>
2021-09-28 17:05:30 +0900

    extra fix of open db

--- Disk Spacer Manager/file.cpp
+++ Disk Spacer Manager/file.cpp
@@ -5,28 +5,42 @@
 #include <unistd.h>
 #include <cerrno>
 #include <cstring>
-#define FILE_SIZE 10*1024*1024
 using namespace std;
 
 // Open existing database file or create one if not existed.
 int file_open_database_file(const char* pathname){
-    int64_t db_file = open(pathname,O_SYNC|O_RDWR);   //Open the file from the path
+    db_file = open(pathname,O_SYNC|O_RDWR);   //Open the file from the path
     if(db_file==-1) { //no file
         db_file = open(pathname,O_SYNC|O_RDWR|O_CREAT);  //Create new file
         if(db_file==-1){
           fprintf(stderr,"Creating file error: %s\n",strerror(errno));
           return -1;
         }
-        if(truncate(pathname,FILE_SIZE)==-1){   //Set file size as 10MB
+
+        if(ftruncate(db_file,FILE_SIZE)==-1){   //Set file size as 10MB
             fprintf(stderr,"Setting file size error: %s\n", strerror(errno));
             return -1;
         }
-        header_page_t * header = new header_page_t; //Create header page
+        
+        header = new header_page_t; //Create header page
+        memset(header,0,PAGE_SIZE);
         header->first_page_num=1;
-        memset(header,0,sizeof(header_page_t));
-        header->pages=(FILE_SIZE-sizeof(header_page_t))/sizeof(page_t);
+        header->pages=(FILE_SIZE-sizeof(header_page_t))/PAGE_SIZE;
+
+        for(int i=1;i<=header->pages;i++){  //paginate the file
+          page_t * free = new page_t;
+          if(i==header->pages){
+            free->next_page_num=0;
+          }else{
+            free->next_page_num=i+1;
+          }
+          pwrite(db_file,free,PAGE_SIZE,i*PAGE_SIZE);
+          delete free;
+        }
         //write header at first page
-        pwrite(db_file,header,sizeof(header_page_t),0);
+        pwrite(db_file,header,PAGE_SIZE,0);
+    }else{
+      pread(db_file,header,PAGE_SIZE,0);   //Read the header
     }
     return db_file;
 }
@@ -55,5 +69,5 @@ void file_write_page(int fd, pagenum_t pagenum, const page_t* src){
 // Stop referencing the database file
 void file_close_database_file(){
   //free
-  //close(db_file);
+  close(db_file);
 }
