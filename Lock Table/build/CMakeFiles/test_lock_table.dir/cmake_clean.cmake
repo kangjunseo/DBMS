bcbbfcc0728089219b56a5311f0fd3d63150fcbe
KJS <88201041+kangjunseo@users.noreply.github.com>
2021-12-25 22:33:13 +0900

    Add files via upload

---
+++ Lock Table/build/CMakeFiles/test_lock_table.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_lock_table.dir/main.cc.o"
+  "CMakeFiles/test_lock_table.dir/main.cc.o.d"
+  "bin/test_lock_table"
+  "bin/test_lock_table.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_lock_table.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lock Table/build/lock_table/CMakeFiles/lock_table.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "../lib/liblock_table.a"
+)
